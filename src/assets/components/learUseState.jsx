2332e30b5f68ca8c98d2970667df7182dc4fa7a7
Amansaxena16 <aman16nov.as@gmail.com>
2024-10-05 12:46:01 +0300

    added new changes

--- src/assets/components/learUseState.jsx
+++ src/assets/components/learUseState.jsx
@@ -91,6 +91,8 @@ function LearnUseState(){
 
             <br />
             <br />
+
+            <h1 className="text-blue-500">The last is form Fedora Linux!!</h1>
             
         </div>
     )
