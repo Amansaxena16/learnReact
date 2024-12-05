8192deb8e0f6e5f0e9835cfa989c1124a9a49e3a
Amansaxena16 <aman16nov.as@gmail.com>
2024-12-05 17:53:56 +0530

    added new file and changes

--- src/assets/components/fun/Fun.jsx
+++ src/assets/components/fun/Fun.jsx
@@ -1,11 +1,18 @@
+import ThemeContextProvider from "../../../context/ThemeContextProvider"
 import Login from "../login/Login"
 import Profile from "../profile/Profile"
+import ThemeBtn from "../themeBtn/ThemeBtn"
+import ThemeCard from "../themeCard/ThemeCard"
 
 function Fun(){
     return(
         <div>
             <Profile/>
             <Login/>
+            <ThemeContextProvider>
+                <ThemeCard/>
+                <ThemeBtn/>
+            </ThemeContextProvider>
         </div>
     )
 }
