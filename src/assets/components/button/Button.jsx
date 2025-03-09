9c8d68fb50a730d35d599b45020fa0a53c62326c
aman.saxena <aman.saxena@einstellen.ai>
2025-03-09 11:19:48 +0530

    Initial a new project

--- src/assets/components/button/Button.jsx
+++
@@ -1,8 +0,0 @@
-function Button(props){
-    return(
-        <button onClick={props.onClick} className="px-3 py-1 m-4 rounded-sm 
-        text-white font-bold" style={{backgroundColor: props.bg}}>{props.value || "Click Me"} </button>
-    )
-}
-
-export default Button
